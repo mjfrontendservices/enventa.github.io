46245255ca4c5ef107319db45014af6495f1e0d3
chemjstore <chemj.mjfrontendservices@gmail.com>
2020-10-02 23:15:55 +0800

    t34t34t3

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    // herop img owl carousel
+
     $('.hero .owl-carousel').owlCarousel({
         autoplay: true,
         autoplayTimeout: 3500,
@@ -24,31 +26,5 @@ $(document).ready(function () {
             }
         }
     })
-
-    // hero img
-
-    // var heroImg;
-    // $.getJSON("assets/json/hero.json", (data) => {
-    //     heroImg = data;
-    //     getHeroImgData()
-    // })
-
-    // function getHeroImgData() {
-    //     var imgSlide = $('.hero');
-    //     heroImg.forEach(hero => {
-    //         imgSlide.append(`
-    //             <div class="imgSlide">
-    //                 <img src="${hero.img}" alt="">
-    //             </div>
-    //         `)
-    //     })
-
-    //     $('.hero').slick({
-    //         slidesToShow: 1,
-    //         slidesToScroll: 1,
-    //         autoplay: true,
-    //         autoplaySpeed: 3000,
-    //     });
-    // }
 
 })
