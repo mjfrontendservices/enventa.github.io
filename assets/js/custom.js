4fa16bbd9555cc76702a8b6b9f960f476724c9f6
chemjstore <chemj.mjfrontendservices@gmail.com>
2020-10-03 18:18:31 +0800

    utyutyutyut

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -27,33 +27,6 @@ $(document).ready(function () {
         }
     })
 
-    // sample slide on product
-
-    $('.modal-body .owl-carousel').owlCarousel({
-        autoplay: true,
-        autoplayTimeout: 3500,
-        autoplayHoverPause: true,
-        loop: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false,
-                loop: true
-            },
-            600: {
-                items: 1,
-                nav: false,
-                loop: true
-            },
-            1000: {
-                items: 1,
-                nav: false,
-                loop: true
-            }
-        }
-    })
-
     // search bar
 
     $(".searchInput").on("keyup", function () {
@@ -63,7 +36,81 @@ $(document).ready(function () {
         });
     });
 
-    // our availab;e product
+    // product modal
+
+    var productModal;
+    $.getJSON("assets/json/productModal.json", (data) => {
+        productModal = data;
+        productModalFunction()
+    })
+
+    function productModalFunction() {
+        var modals = $('.productModals');
+        productModal.forEach(productModal => {
+            modals.append(`
+            <div id="${productModal.productIdTarget}" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+        
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="owl-carousel">
+                                <img src="${productModal.img1}" alt="">
+                                <img src="${productModal.img2}" alt="">
+                                <img src="${productModal.img3}" alt="">
+                                <img src="${productModal.img4}" alt="">
+                                <img src="${productModal.img5}" alt="">
+                            </div>
+                            <h6>${productModal.productName}</h6>
+                            <p class="price">${productModal.price}</p>
+                            <p>
+                                <span>
+                                    ${productModal.description}
+                                </span></br>
+                                <a href="sms: 09978972884?body=Hi, I want to orer ${productModal.productName}">
+                                    <button class="inquire">Order Now!</button>
+                                </a>
+                            </p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Back</button>
+                        </div>
+                    </div>
+        
+                </div>
+            </div>
+            `)
+            $('.modal-body .owl-carousel').owlCarousel({
+                autoplay: true,
+                autoplayTimeout: 3500,
+                autoplayHoverPause: true,
+                loop: true,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                        nav: false,
+                        loop: true
+                    },
+                    600: {
+                        items: 1,
+                        nav: false,
+                        loop: true
+                    },
+                    1000: {
+                        items: 1,
+                        nav: false,
+                        loop: true
+                    }
+                }
+            })
+        })
+    }
+
+    // our available product
 
     var allProduct;
     $.getJSON("assets/json/items.json", (data) => {
